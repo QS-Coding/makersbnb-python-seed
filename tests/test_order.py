1a536c3c18e78e1e744abea9db6a1fd31b759751
Will <williama149@gmail.com>
2024-07-08 14:47:57 +0100

    added booking tests

--- tests/test_order.py
+++
@@ -1,5 +0,0 @@
-from lib.models.booking import *
-from datetime import datetime
-
-# def test_booking_init():
-#     booking = Booking(1, 1, datetime(2024, 1, 1), datetime(2024, 1, 3), True, datetime(2024, 1, 1))
