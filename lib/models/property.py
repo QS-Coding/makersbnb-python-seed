c676082e7322bbc54ccef9b52aa04f410b0aa16f
Will <williama149@gmail.com>
2024-07-08 14:37:52 +0100

    Added available dates to property models

--- lib/models/property.py
+++ lib/models/property.py
@@ -1,14 +1,16 @@
 class Property():
     
-    def __init__(self, id, address, price, description, user_id) -> None:
+    def __init__(self, id, address, price, description, available_from, available_to, user_id) -> None:
         self.id = id
         self.address = address
         self.price = price
         self.description = description
+        self.available_from = available_from
+        self.available_to = available_to
         self.user_id = user_id
     
     def __repr__(self) -> str:
-        return f'Property({self.id}, {self.address}, {self.price}, {self.description}, {self.user_id})'
+        return f'Property({self.id}, {self.address}, {self.price}, {self.description}, {self.available_from}, {self.available_to}, {self.user_id})'
     
     def __eq__(self, value: object) -> bool:
         return self.__dict__ == value.__dict__
