5586b4aa391534b980e2c0f5705a69190dcbe81f
Oleg Novikov <o.novikov@ymail.com>
2024-07-09 14:14:27 +0100

    Initial commit on list owners properties feature

--- lib/repositories/property_repository.py
+++ lib/repositories/property_repository.py
@@ -23,3 +23,11 @@ class PropertyRepository:
             row = rows[0]
             return Property(row['id'], row['name'], row['description'], row['price'], row['available_from'], row['available_to'], row['owner_id'])
         return False
+    
+    def find_by_owner_id(self, owner_id):
+        rows = self._connection.execute('SELECT * FROM properties WHERE owner_id = %s', [owner_id])
+        properties = []
+        for row in rows:
+            item = Property(row['id'], row['name'], row['description'], row['price'], row['available_from'], row['available_to'], row['owner_id'])
+            properties.append(item)
+        return properties
