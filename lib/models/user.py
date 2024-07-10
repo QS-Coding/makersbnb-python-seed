2a5ea116de5d6b6549d4c9a601786b10bb5e7d01
Will <williama149@gmail.com>
2024-07-10 11:58:49 +0100

    Added error handling for user model

--- lib/models/user.py
+++ lib/models/user.py
@@ -10,3 +10,22 @@ class User():
     
     def __eq__(self, value: object) -> bool:
         return self.__dict__ == value.__dict__
+    
+    def is_valid(self):
+        if self.email == None or self.email == '':
+            return False
+        if self.name == None or self.name == '':
+            return False
+        if self.password == None or self.password == '':
+            return False
+        return True
+    
+    def generate_errors(self):
+        errors = []
+        if self.email == None or self.email == '':
+            errors.append('You need to enter a name')
+        if self.name == None or self.name == '':
+            errors.append('You need to enter an email')
+        if self.password == None or self.password == '':
+            errors.append('You need to enter a password')
+        return ', '.join(errors)
--- tests/test_user.py
+++ tests/test_user.py
@@ -14,3 +14,20 @@ def test_user_str_rep():
 def test_eq():
     user = User(1, 'example@email.com', 'First Last', 'password123')
     assert user == User(1, 'example@email.com', 'First Last', 'password123')
+
+def test_is_valid():
+    user = User(1, 'example@email.com', 'First Last', 'password123')
+    user2 = User(2, '', 'First Last', 'password123')
+    user3 = User(3, 'example@email.com', None, 'password123')
+    user4 = User(3, 'example@email.com', 'First Last', None)
+    assert user.is_valid() == True
+    assert user2.is_valid() == False
+    assert user3.is_valid() == False
+    assert user4.is_valid() == False
+
+def test_generate_errors():
+    user = User(1, '', 'First Last', 'password123')
+    user2 = User(2, 'example@email.com', '', None)
+    assert user.generate_errors() == 'You need to enter a name'
+    assert user2.generate_errors() == 'You need to enter an email, You need to enter a password'
+
